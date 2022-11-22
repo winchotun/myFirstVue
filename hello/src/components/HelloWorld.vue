f563d90c5089bffb1d9c46fcab09e604cc1f7f63
Win Cho Tun <mr.winchotun@gmail.com>
2022-11-22 16:33:26 +0900

    put i got it

--- hello/src/components/HelloWorld.vue
+++ hello/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>This is for testing</h2>
     <h3>
-      hi this is win cho tun
+      I got it
     </h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
